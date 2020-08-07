1a1dae821b36b034cd077b6fd8db002131a54402
PatrickAlphaC <collinspatrickkevin@gmail.com>
2020-08-06 23:51:05 -0400

    updated remix for some permissions

--- ethereum/remix_folder/lottery_man.sol
+++ ethereum/remix_folder/lottery_man.sol
@@ -4,7 +4,7 @@ import {Randomness_interface} from "./Randomness_interface.sol";
 import {governance_interface} from "./governance_interface.sol";
 
 contract Lottery is ChainlinkClient {
-    enum LOTTERY_STATE { OPEN, CLOSED, CALCULATING_WINNER }
+    enum LOTTERY_STATE {OPEN, CLOSED, CALCULATING_WINNER}
     LOTTERY_STATE public lottery_state;
     uint256 public lotteryId;
     address payable[] public players;
@@ -16,9 +16,8 @@ contract Lottery is ChainlinkClient {
     // Alarm stuff
     address CHAINLINK_ALARM_ORACLE = 0xc99B3D447826532722E41bc36e644ba3479E4365;
     bytes32 CHAINLINK_ALARM_JOB_ID = "2ebb1c1a4b1e4229adac24ee0b5f784f";
-    
-    constructor(address _governance) public
-    {
+
+    constructor(address _governance) public {
         setPublicChainlinkToken();
         lotteryId = 1;
         lottery_state = LOTTERY_STATE.CLOSED;
@@ -29,20 +28,31 @@ contract Lottery is ChainlinkClient {
         assert(msg.value == MINIMUM);
         assert(lottery_state == LOTTERY_STATE.OPEN);
         players.push(msg.sender);
-    } 
-    
-  function start_new_lottery(uint256 duration) public {
-    require(lottery_state == LOTTERY_STATE.CLOSED, "can't start a new lottery yet");
-    lottery_state = LOTTERY_STATE.OPEN;
-    Chainlink.Request memory req = buildChainlinkRequest(CHAINLINK_ALARM_JOB_ID, address(this), this.fulfill_alarm.selector);
-    req.addUint("until", now + duration);
-    sendChainlinkRequestTo(CHAINLINK_ALARM_ORACLE, req, ORACLE_PAYMENT);
-  }
-  
-  function fulfill_alarm(bytes32 _requestId)
-    public
-    recordChainlinkFulfillment(_requestId)
-      {
+    }
+
+    function start_new_lottery(uint256 duration) public {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "can't start a new lottery yet"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+        Chainlink.Request memory req = buildChainlinkRequest(
+            CHAINLINK_ALARM_JOB_ID,
+            address(this),
+            this.fulfill_alarm.selector
+        );
+        req.addUint("until", now + duration);
+        sendChainlinkRequestTo(CHAINLINK_ALARM_ORACLE, req, ORACLE_PAYMENT);
+    }
+
+    function fulfill_alarm(bytes32 _requestId)
+        public
+        recordChainlinkFulfillment(_requestId)
+    {
+        require(
+            lottery_state == LOTTERY_STATE.OPEN,
+            "The lottery hasn't even started!"
+        );
         // add a require here so that only the oracle contract can
         // call the fulfill alarm method
         lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
@@ -50,15 +60,23 @@ contract Lottery is ChainlinkClient {
         pickWinner();
     }
 
-
     function pickWinner() private {
-        require(lottery_state == LOTTERY_STATE.CALCULATING_WINNER, "You aren't at that stage yet!");
-        Randomness_interface(governance.randomness()).getRandom(lotteryId, lotteryId);
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't at that stage yet!"
+        );
+        Randomness_interface(governance.randomness()).getRandom(
+            lotteryId,
+            lotteryId
+        );
         //this kicks off the request and returns through fulfill_random
     }
-    
+
     function fulfill_random(uint256 randomness) external {
-        require(lottery_state == LOTTERY_STATE.CALCULATING_WINNER, "You aren't at that stage yet!");
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't at that stage yet!"
+        );
         require(randomness > 0, "random-not-found");
         // assert(msg.sender == governance.randomness());
         uint256 index = randomness % players.length;
@@ -67,15 +85,15 @@ contract Lottery is ChainlinkClient {
         lottery_state = LOTTERY_STATE.CLOSED;
         // You could have this run forever
         // start_new_lottery();
-        // or with a cron job from a chainlink node would allow you to 
+        // or with a cron job from a chainlink node would allow you to
         // keep calling "start_new_lottery" as well
     }
 
     function get_players() public view returns (address payable[] memory) {
         return players;
     }
-    
-    function get_pot() public view returns(uint256){
+
+    function get_pot() public view returns (uint256) {
         return address(this).balance;
     }
 }
