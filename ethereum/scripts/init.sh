eaabe0e0eb5fbb7520b1e78046730eddf4fade6b
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-01-26 13:30:21 +0100

    Create init.sh

---
+++ ethereum/scripts/init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+/*
+set -e
+
+docker-compose up -d
+cd ethereum
+npx truffle migrate --network ganache
+npx truffle exec scripts/fund-contract.js --network ganache
+npx truffle exec scripts/fund-operator.js --network ganache
+npx truffle exec scripts/approve-operator.js --network ganache
+*/
